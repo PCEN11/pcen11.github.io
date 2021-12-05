500035f36906d7304409db9f9e94dfacc26975e0
PCEN11 <93833479+PCEN11@users.noreply.github.com>
2021-12-05 17:27:10 +0800

    Add files via upload

---
+++ js/time_zk.js
@@ -0,0 +1,18 @@
+function show_date_time(){
+window.setTimeout("show_date_time()", 1000);
+BirthDay=new Date("1/11/2022 08:00:00");
+today=new Date();
+timeold=(BirthDay.getTime()-today.getTime());
+sectimeold=timeold/1000
+secondsold=Math.floor(sectimeold);
+msPerDay=24*60*60*1000
+e_daysold=timeold/msPerDay
+daysold=Math.floor(e_daysold);
+e_hrsold=(e_daysold-daysold)*24;
+hrsold=Math.floor(e_hrsold);
+e_minsold=(e_hrsold-hrsold)*60;
+minsold=Math.floor((e_hrsold-hrsold)*60);
+seconds=Math.floor((e_minsold-minsold)*60);
+span_dt_dt.innerHTML="<font size=53px>距期末考还有：</font><br/><br/><font size=8 face='ff' color=red>"+daysold+"天"+hrsold+"小时"+minsold+"分"+seconds+"秒"+"<br></font><br>" ; // 这里你自己改。就是倒计时的样式，字体大小size=4 颜色是color
+}
+show_date_time();
